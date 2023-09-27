1e8dfda9a3bc99d0b52e7513453c74a0ed3d7a13
Peter Eisenmann <p3732@getgoogleoff.me>
2023-09-27 19:59:05 +0200

    user: use AdwSwitchRow

--- src/ui/pages/user.py
+++ src/ui/pages/user.py
@@ -12,15 +12,16 @@ class UserPage(Gtk.Box, Page):
     image = 'user-symbolic'
 
     user_name_row = Gtk.Template.Child()
-    autologin_switch = Gtk.Template.Child()
+    autologin_row = Gtk.Template.Child()
     password_row = Gtk.Template.Child()
     continue_button = Gtk.Template.Child()
 
     def __init__(self, **kwargs):
         Gtk.Box.__init__(self, **kwargs)
 
-    def _set_continue_button(self, autologin):
+    def _set_continue_button(self):
         has_user_name = not self.user_name_row.get_text().strip() == ''
+        autologin = self.autologin_row.get_active()
         has_password = not self.password_row.get_text() == ''
         can_continue = has_user_name and (autologin or has_password)
         self.continue_button.set_sensitive(can_continue)
@@ -28,12 +29,8 @@ class UserPage(Gtk.Box, Page):
     ### callbacks ###
 
     @Gtk.Template.Callback('autologin_row_clicked')
-    def _autologin_row_clicked(self, row):
-        self.autologin_switch.activate()
-
-    @Gtk.Template.Callback('autologin_switch_flipped')
-    def _autologin_switch_flipped(self, autologin_switch, state):
-        self._set_continue_button(state)
+    def _autologin_row_clicked(self, row, state):
+        self._set_continue_button()
 
     @Gtk.Template.Callback('focus_password')
     def _focus_password(self, row):
@@ -41,7 +38,7 @@ class UserPage(Gtk.Box, Page):
 
     @Gtk.Template.Callback('entry_changed')
     def _entry_changed(self, editable):
-        self._set_continue_button(self.autologin_switch.get_state())
+        self._set_continue_button()
 
     @Gtk.Template.Callback('continue')
     def _continue(self, object):
@@ -53,4 +50,4 @@ class UserPage(Gtk.Box, Page):
     def unload(self):
         global_state.set_config('user_name', self.user_name_row.get_text().strip())
         global_state.set_config('user_password', self.password_row.get_text())
-        global_state.set_config('user_autologin', self.autologin_switch.get_state())
+        global_state.set_config('user_autologin', self.autologin_row.get_active())
