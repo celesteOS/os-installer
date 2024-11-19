8a7fac19f7d35e35611672398384a8bc3ab5540c
Peter Eisenmann <p3732@getgoogleoff.me>
2024-11-20 10:42:56 +1300

    Add NoEfiPartitonRow
    Will be used in future disk page rework.

--- src/ui/widgets.py
+++ src/ui/widgets.py
@@ -131,6 +131,14 @@ class MultiSelectionRow(Adw.ComboRow):
                 return
 
 
+@Gtk.Template(resource_path='/com/github/p3732/os-installer/ui/widgets/no_efi_partition_row.ui')
+class NoEfiPartitionRow(Adw.ActionRow):
+    __gtype_name__ = __qualname__
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+
 @Gtk.Template(resource_path='/com/github/p3732/os-installer/ui/widgets/progress_row.ui')
 class ProgressRow(Adw.ActionRow):
     __gtype_name__ = __qualname__
