6e5d3c5abc8784fd1c6ccb35a0aefc646e4101b8
Peter Eisenmann <p3732@getgoogleoff.me>
2024-12-06 01:17:09 -0300

    Add and use ConfigTranslation context manager

--- src/ui/pages/welcome.py
+++ src/ui/pages/welcome.py
@@ -1,11 +1,12 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-import locale
+from locale import gettext as _
 from pathlib import Path
 
 from gi.repository import Gtk
 
 from .config import config
+from .config_translation import config_translation
 from .welcome_provider import welcome_provider
 
 
@@ -24,9 +25,8 @@ class WelcomePage(Gtk.Box):
         welcome = config.get('welcome_page')
 
         if text := welcome.get('text', None):
-            locale.textdomain('os-installer-config')
-            text = locale.gettext(text)
-            locale.textdomain('os-installer')
+            with config_translation:
+                text = _(text)
         else:
             text = self.description.get_label()
             text = text.format(config.get('distribution_name'))
