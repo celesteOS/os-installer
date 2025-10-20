a6afca9d73060fde62b42efe4c0c853697a30363
Peter Eisenmann <p3732@getgoogleoff.me>
2025-10-20 19:39:27 +0200

    summary-row: Bundle update functionality
    This allows summary rows to be used on multiple pages without duplicated
    update functionality.

--- src/ui/row/summary_row.py
+++ src/ui/row/summary_row.py
@@ -1,11 +1,79 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from gi.repository import Adw, Gtk
+from gi.repository import Adw, GObject, Gtk
+
+from .config import config
+from .state_machine import state_machine
+from .translator import config_gettext
+
+
+def _get_choices(choices):
+    _ = config_gettext
+    choice_texts = []
+    for choice in choices:
+        if choice.options:
+            if choice.state.keyword:
+                text = f"{_(choice.name)} – {_(choice.state.display)}"
+                choice_texts.append(text)
+        elif choice.state:
+            choice_texts.append(_(choice.name))
+
+    if choice_texts:
+        return ", ".join(choice_texts)
+    else:
+        # Translators: Shown when list of selected software is empty.
+        return _("None")
+
+
+update_funcs = {
+    "desktop": ("desktop_chosen", lambda desktop: desktop[1]),
+    "feature": ("feature_choices", _get_choices),
+    "format": ("formats", lambda formats: formats[1]),
+    "keyboard-overview": ("keyboard_layout", lambda keyboard: keyboard[1]),
+    "language": ("language_chosen", lambda language: language.name),
+    "timezone": ("timezone", lambda timezone: timezone),
+    "software": ("software_choices", _get_choices),
+}
+
+titles = {
+    # Translators: Description of selected desktop.
+    "desktop": _("Desktop"),
+    # Translators: Description of selected additional features.
+    "feature": _("Additional Features"),
+    # Translators: Description of selected format.
+    "format": _("Formats"),
+    # Translators: Description of selected keyboard layout.
+    "keyboard-overview": _("Keyboard Layout"),
+    # Translators: Description of selected language.
+    "language": _("Language"),
+    # Translators: Description of selected timezone.
+    "timezone": _("Timezone"),
+    # Translators: Description of selected additional software.
+    "software": _("Additional Software"),
+}
 
 
 @Gtk.Template(resource_path='/com/github/p3732/os-installer/ui/row/summary_row.ui')
 class SummaryRow(Adw.ActionRow):
     __gtype_name__ = __qualname__
 
+    page_name = GObject.Property(type=str)
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
+        # page-name is not set yet, react when that happens
+        self.connect("notify::page-name", self._set_update)
+
+    def _set_update(self, row, value):
+        page_name = self.get_property("page-name")
+
+        if not state_machine.is_page_available(page_name):
+            self.set_visible(False)
+            return
+
+        title = titles.get(page_name, " ")
+        self.set_title(_(title))
+
+        config_var, subtitle_func = update_funcs.get(page_name, ("", None))
+        config.subscribe(config_var, lambda v: self.set_subtitle(subtitle_func(v)))
